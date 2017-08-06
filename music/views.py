cd1ec4dfeb7332f21b2468e32adfe6aefc203518
Narongsak <narongsak.chobsri@gmail.com>
2017-08-07 00:47:33 +0700

    Add http404 does not exist

--- music/views.py
+++ music/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
@@ -10,4 +10,8 @@ def index(request):
  	return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-	return HttpResponse("<h2>Details for Album id : " + str(album_id) + "</h2>")
+	try:
+		album = Album.objects.get(pk=album_id)
+	except Album.DoesNotExist:
+		raise Http404("Album does not exist")
+	return render(request, 'music/detail.html', { 'album': album })
